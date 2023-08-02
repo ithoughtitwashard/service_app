c98a49c97511097ad944baff85f09ebfdeea6e5a
IgorSS <igorskor0912@gmail.com>
2023-08-02 21:53:41 +0300

    Create 'set_price' celery-task

--- service/celery_app.py
+++ service/celery_app.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-from celery import Celery
+from celery import Celery, shared_task
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service.settings')
@@ -12,7 +12,6 @@ app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
 
 
-@app.task
+@shared_task
 def debug_task():
-    time.sleep(20)
     print('Debug task msg')
